a8b85800da3dd997e4b9a0401e916583d4b29b38
Daryl <clarinojd@gmail.com>
2021-04-13 05:59:58 +0800

    Allow blob public access

--- storage-account.tf
+++ storage-account.tf
@@ -7,6 +7,7 @@ resource "azurerm_storage_account" "rocalc" {
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
 
+  allow_blob_public_access = true
   static_website {
     index_document     = "index.html"
     error_404_document = "index.html"
